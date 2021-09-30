c6c2c24124af0cdf3c33bbf0b7d1a7f53f58d8b3
ShreyaaChauhan <shreyachauha1113@gmail.com>
2021-10-01 00:59:22 +0200

    completed

--- Tea-Station-Project/app.js
+++ Tea-Station-Project/app.js
@@ -1,11 +1,21 @@
+// setup date
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+// setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
-
+const navlink = document.getElementById("nav-link");
+// show nav
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
 });
-
+// close nav
 navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+// close nav
+navlink.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
